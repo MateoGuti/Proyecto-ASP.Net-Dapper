3a21f8926a004376c2c54c49c40af2011ff70269
mateo <jaisongutierrez1908@gmail.com>
2024-03-07 21:55:37 -0600

    P

---
+++ Subsistema_Estudiantil/Controllers/ControladorPortafolio.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Subsistema_Estudiantil.Controllers
+{
+    public class ControladorPortafolio : Controller
+    {
+        public IActionResult Portafolio()
+        {
+            return View();
+        }
+
+        public IActionResult PresentacionPortafolio()
+        {
+            return View();
+        }
+    }
+}
